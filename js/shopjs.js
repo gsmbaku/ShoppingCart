704aed99856778e7eb321f73c0df679aea32b838
Patrick Ford <patrickford@msn.com>
2015-08-05 19:46:29 -0700

    Refactoring

--- js/shopjs.js
+++ js/shopjs.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('#add').click(function() {
     var thing = $("#input").val();    
     $(".items").append("<li>"+thing+" <button class='remove'> Remove</button></li>");
+    $("#input").val("");
   })
 
 
